80d8b2f7832bfdcfaedc972e098407831a694ad3
Nupur Kinger <nupur.2706@gmail.com>
2020-11-16 16:32:07 +0700

    Adding increase and decrease arrow to checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,11 +2,27 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemsToAdd.id);
 
     if (existingCartItem) {
-        return cartItems.map(item => 
-            (item.id === cartItemsToAdd.id) 
-                ? {...item, quantity: item.quantity + 1}
+        return cartItems.map(item =>
+            (item.id === cartItemsToAdd.id)
+                ? { ...item, quantity: item.quantity + 1 }
                 : item)
     }
 
-    return [...cartItems, {...cartItemsToAdd, quantity: 1}];
+    return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
+};
