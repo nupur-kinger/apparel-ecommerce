8e07f769677a5f9fec4660f98b99c6ad24fce114
Nupur Kinger <nupur.2706@gmail.com>
2020-11-16 11:03:00 +0700

    Added redux to App.js and header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,26 +5,32 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.util';
 
-const HEADER = ({currentUser}) => (
+import { connect } from 'react-redux';
+
+const HEADER = ({ currentUser }) => (
     <div className='header'>
         <Link to='/' className="logo-container">
             <Logo className='logo' />
         </Link>
         <div className='options'>
-        <Link to='/shop' className='option'>SHOP</Link>    
-        <Link to='/contact' className='option'>CONTACT</Link>    
-        {
-            currentUser
-            ? <div className='option' onClick={
-                () => {
-                    auth.signOut();
-                    console.log("Signed out");
-                }
-            }>SIGN OUT</div>
-            : <Link className='option' to='/signIn'>SIGN IN</Link>
-        }
-       </div>
+            <Link to='/shop' className='option'>SHOP</Link>
+            <Link to='/contact' className='option'>CONTACT</Link>
+            {
+                currentUser
+                    ? <div className='option' onClick={
+                        () => {
+                            auth.signOut();
+                            console.log("Signed out");
+                        }
+                    }>SIGN OUT</div>
+                    : <Link className='option' to='/signIn'>SIGN IN</Link>
+            }
+        </div>
     </div>
 );
 
-export default HEADER;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(HEADER);
