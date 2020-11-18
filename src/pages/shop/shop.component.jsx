502fc6d4a3bb34222c350466cac78a4dc830ea4c
Nupur Kinger <nupur.2706@gmail.com>
2020-11-18 15:21:50 +0700

    Changed shop data to a promise

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -26,7 +26,7 @@ class ShopPage extends React.Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot((snapshot) => {
+        this.unsubscribeFromSnapshot = collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({ loading: false });
